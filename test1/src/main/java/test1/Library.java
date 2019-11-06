3de2525ca90cd878858c5bdebf482faae9d9d78f
whnnn30 <whnnn30@gmail.com>
2019-11-06 20:20:23 +0800

    RedSox push

--- test1/src/main/java/test1/Library.java
+++ test1/src/main/java/test1/Library.java
@@ -1,6 +1,7 @@
 public class Library {
     public static void main(String arg[]) {
     	System.out.println("When there is a fire, commit and push");
+    	System.out.println("Add some changes");
     }
     
     public boolean someLibraryMethod() {
