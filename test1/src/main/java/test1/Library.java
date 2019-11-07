5495c705e931f2863d90f3b93c25a75268bf2009
whnnn30 <whnnn30@gmail.com>
2019-11-07 19:22:17 +0800

    RedSox commit for task_3

--- test1/src/main/java/test1/Library.java
+++ test1/src/main/java/test1/Library.java
@@ -2,6 +2,7 @@ public class Library {
     public static void main(String arg[]) {
     	System.out.println("When there is a fire, commit and push");
     	System.out.println("Add some changes");
+    	System.out.println("Add some changes for task 3");
     }
     
     public boolean someLibraryMethod() {
